e8622038bb01b3c7dc53b855336000944a8cbff3
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-03-22 13:37:18 -0500

    Added initial Spring Boot project. Closes #1

---
+++ src/test/java/austinspinazze/springframework/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package austinspinazze.springframework.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
